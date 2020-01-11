45779b2353eedfd7ad1eb03517d3f6703d25b0eb
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-11 20:09:33 +0100

    Corrects the now deprecated instanceMethods

--- auth-system/models/user.js
+++ auth-system/models/user.js
@@ -26,13 +26,12 @@ var User = sequelize.define('users', {
             const salt = bcrypt.genSaltSync();
             user.password = bcrypt.hashSync(user.password, salt);
         }
-    },
-    instanceMethods: {
-        validPassword: function(password) {
-            return bcrypt.compareSync(password, this.password);
-        }
     }
 });
+
+User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 // create all the defined tables in the specified database.
 sequelize.sync()
