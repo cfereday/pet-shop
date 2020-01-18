81f6345c4516c55d78d82d6b671ea5fcdc77b5eb
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-18 13:33:41 +0100

    Adds the post to save users passwords
    * Clearly this is totally insecure, sending string passwords in a post request

--- user-authentication/server.js
+++ user-authentication/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const users = [];
 
 app.get('/users', (req, res) => {
     res.json(users);
+});
+
+app.post('/users', (req, res) => {
+    const user = { name: req.body.name, password: req.body.password };
+    users.push(user);
+    res.status(201).send();
 });
 
 app.listen(3000);
