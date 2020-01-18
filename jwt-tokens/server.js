951ca199e7fd0974a97aecfab96552cf1d4980a6
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-18 18:24:14 +0100

    Adds JWT authentication

--- jwt-tokens/server.js
+++ jwt-tokens/server.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const jwt = require('jsonwebtoken');
+
+//allows app to use json from body that gets passed in from request
+app.use(express.json());
 
 const posts = [
     {
@@ -15,4 +21,20 @@ const posts = [
 app.get('/posts', (req, res) => {
     res.json(posts);
 });
+
+app.post('/login', (req, res) => {
+    // authenticate user checking password done here
+
+    //authenticate & use JWT
+    const username = req.body.username;
+    const user = { name: username };
+
+    // payload = what we want to serialise
+    // secret key to serialise
+    //can also add expiration date
+    // accessToken will have user info saved in it
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json( { accessToken: accessToken });
+});
+
 app.listen(3000);
