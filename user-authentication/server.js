bab5ee1ecbefaa877f8dbc29d446dcdf529c34dd
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-18 14:27:51 +0100

    Bcrypt can directly create salt, refactors into more elegant 1 step

--- user-authentication/server.js
+++ user-authentication/server.js
@@ -13,9 +13,7 @@ app.get('/users', (req, res) => {
 app.post('/users', async (req, res) => {
     try {
         //by default genSalt is 10- can add in rounds a number of entropy you'd like
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        console.log('salt', salt);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         console.log('hashed', hashedPassword);
         const user = { name: req.body.name, password: hashedPassword };
         users.push(user);
