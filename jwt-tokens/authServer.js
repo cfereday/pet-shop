c710f17002670b29cb6686d8a6bec3c9d65be569
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-18 19:16:49 +0100

    Removes non login related code

--- jwt-tokens/authServer.js
+++ jwt-tokens/authServer.js
@@ -7,20 +7,20 @@ const jwt = require('jsonwebtoken');
 //allows app to use json from body that gets passed in from request
 app.use(express.json());
 
-const posts = [
-    {
-        username: 'Charlotte',
-        title: 'Post 1'
-    },
-    {
-        username: 'Felipe',
-        title: 'Post 2'
-    }
-];
+app.post('/login', (req, res) => {
+    // authenticate user checking password done here
 
-app.get('/posts', authenticateToken, (req, res) => {
-    console.log('req.user.name', req.user.name);
-    res.json(posts.filter(post => post.username === req.user.name));
+    //authenticate & use JWT
+    const username = req.body.username;
+    //value serialised
+    const user = { name: username };
+
+    // payload = what we want to serialise
+    // secret key to serialise
+    //can also add expiration date
+    // accessToken will have user info saved in it
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json( { accessToken: accessToken });
 });
 
 function authenticateToken(req, res, next) {
