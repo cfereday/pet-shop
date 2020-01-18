93ee276ab86be68470e8ea11b2d78d6bf5c00bf1
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-18 14:06:55 +0100

    The post uses bcrypt to salt & hash the password
    * As this is asynch placed inside try / catch block

--- user-authentication/server.js
+++ user-authentication/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 
@@ -9,10 +10,19 @@ app.get('/users', (req, res) => {
     res.json(users);
 });
 
-app.post('/users', (req, res) => {
-    const user = { name: req.body.name, password: req.body.password };
-    users.push(user);
-    res.status(201).send();
+app.post('/users', async (req, res) => {
+    try {
+        //by default genSalt is 10- can add in rounds a number of entropy you'd like
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        console.log('salt', salt);
+        console.log('hashed', hashedPassword);
+        const user = { name: req.body.name, password: hashedPasword };
+        users.push(user);
+        res.status(201).send();
+    } catch {
+        req.status(500).send();
+    }
 });
 
 app.listen(3000);
