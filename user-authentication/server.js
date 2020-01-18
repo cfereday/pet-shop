ccb7c11088a2f67b1969cc26675c5db8f958be85
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-18 14:35:33 +0100

    Creates a login endpoint for the user
    - Uses bcrypt.compare to guard against timing attacks

--- user-authentication/server.js
+++ user-authentication/server.js
@@ -15,11 +15,29 @@ app.post('/users', async (req, res) => {
         //by default genSalt is 10- can add in rounds a number of entropy you'd like
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         console.log('hashed', hashedPassword);
-        const user = { name: req.body.name, password: hashedPassword };
+        const user = {name: req.body.name, password: hashedPassword};
         users.push(user);
         res.status(201).send('created password')
-    } catch(err) {
-        console.log('err',err);
+    } catch (err) {
+        console.log('err', err);
+        req.status(500).send(JSON.stringify(err));
+    }
+});
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+        //compare 2 passwords - get salt & make sure both versions equal exact same
+        if (bcrypt.compare(req.body.password, user.password)) {
+            res.send('success logged in')
+        } else {
+            res.send('not logged in')
+        }
+    } catch (err) {
+        console.log('err', err);
         req.status(500).send(JSON.stringify(err));
     }
 });
