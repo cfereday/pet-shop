e9a2224f6216d1a124080fdbeaa1a13c38020cde
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-01-26 13:17:23 +0100

    Creates a shop db when the db is started

--- shop/server.js
+++ shop/server.js
@@ -1,25 +1,7 @@
-const {createDb, migrate} = require('postgres-migrations');
 const express = require('express');
 const app = express();
-require('dotenv').config();
 
 app.use(express.json());
-async function startDb() {
-    const dbConfig = {
-        database: 'shop',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        host: 'localhost',
-        port: 5432
-    };
-
-    await createDb('shop', {
-        ...dbConfig,
-        defaultDatabase: "postgres",
-    });
-    await migrate(dbConfig, "store/postgres/0001.create-schema.sql");
-};
-
 app.set('port', process.env.PORT);
 
 app.get('/', (req, res) => {
--- shop/store/postgres/0001.create-schema.sql
+++
@@ -1,8 +0,0 @@
-START TRANSACTION;
-
-CREATE TABLE IF NOT EXISTS registration (
- id int not null,
- username text not null,
- password int not null
-);
-
